0f5133215cc1db93240ce0041636a95223384f12
Milana8 <91amilana@gmail.com>
2020-07-16 22:21:22 +0200

    Adding mathods: CheckDirection and Bridge Stopwatch

--- DAN_XLVII_Milana_Arnautovic/Zadatak_1/Program.cs
+++ DAN_XLVII_Milana_Arnautovic/Zadatak_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,57 +10,100 @@ namespace Zadatak_1
 {
     class Program
     {
-        public static Random random = new Random();
-        public static List<string> cars = new List<string>();
-        public static int a = random.Next(1, 16);
+        static Random random = new Random();
+        static List<Thread> cars = new List<Thread>();
+        static int a = random.Next(1, 16);
+        static CountdownEvent countdown = new CountdownEvent(1);
 
-        public static void Bridge()
+        static bool CheckDirection(string dir1, string dir2)
         {
+            if ((dir1.Contains("north") && (dir2.Contains("north")) || (dir1.Contains("south") && (dir2.Contains("south")))))
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static void Bridge(Thread thread)
+        {
+            Console.WriteLine(thread.Name + " the car started to cross the bridge");
+            Thread.Sleep(500);
+            Console.WriteLine(thread.Name + " has crossed the bridge.");
+
 
         }
 
-        public static void Directions()
-        {
-            string direction1 = "south";
-            string direction2 = "north";
-            string[] directions = { direction1, direction2 };
-            int d = random.Next(0, 2);
-            Console.WriteLine(Thread.CurrentThread.Name + " going in the direction: " + directions[d] + ".");
-
-
-        }
         public static void TotalCars()
         {
             Console.WriteLine("The total number of cars crossing the bridge is " + a + ".\n");
         }
-        
+
+
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             Thread total = new Thread(new ThreadStart(TotalCars));
             total.Start();
             total.Join();
 
+            string direction1 = "south";
+            string direction2 = "north";
+            string[] directions = { direction1, direction2 };
+
             for (int i = 0; i < a; i++)
             {
-                Thread thread = new Thread(Directions) //Creating threads
+                int d = random.Next(0, 2);
+                string carNum = (i + 1).ToString();
+
+                Thread thread = new Thread(() => Bridge(Thread.CurrentThread))
                 {
 
-                    Name = String.Format("Car_{0}", i + 1) //Naming threads
+                    Name = string.Format("Car " + carNum + " going in the direction: " + directions[d] + ".")
                 };
-                thread.Start();//Starting threads
-                thread.Join();
-
+                cars.Add(thread);
             }
-           
-            Thread direc = new Thread(new ThreadStart(Bridge));
-            direc.Start();
-            direc.Join();
+            foreach (Thread item in cars)
+            {
+                Console.WriteLine(item.Name);
+            }
 
+            for (int i = 0; i < cars.Count; i++)
+            {
+                cars[i].Start();
+                if ((i < cars.Count - 1) && CheckDirection(cars[i].Name, cars[i + 1].Name) == false)
+                {
 
-            
+                    Console.WriteLine(cars[i + 1].Name + " the car is waiting for the bridge to be free.", cars[i + 1].Name);
+                    
+                    cars[i].Join();
 
+                }
+
+                else if (cars[i] == cars[cars.Count - 1])
+                {
+
+                    cars[i].Join();
+                    countdown.Signal();
+                }
+            }
+
+            if (countdown.IsSet)
+            {
+                sw.Stop();
+                Console.WriteLine("The application is running: " + sw.Elapsed.TotalMilliseconds + " milliseconds");
+            }
             Console.ReadLine();
-
         }
     }
 }
+
+
+
+
+
